29f3a1e5c8d20f07cc86bd326fc7b769d18f81f8
zhangys <zhangys@i-boss.cn>
2021-04-26 09:35:09 +0800

    :tada: config

--- product-application/src/main/java/com/example/product/application/client/impl/ProductServiceFallback.java
+++ product-application/src/main/java/com/example/product/application/client/impl/ProductServiceFallback.java
@@ -1,12 +1,15 @@
 package com.example.product.application.client.impl;
 
 import com.example.product.application.client.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ProductServiceFallback implements ProductService {
     @Override
     public String get(long productId) {
-        throw new RuntimeException("product get timeout");
+        log.error("product get timeout productId={}", productId);
+        return "product get timeout";
     }
 }
